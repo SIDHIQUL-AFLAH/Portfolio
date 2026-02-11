79d85544854958c329537756c9ebdb501ac8e23f
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-11 12:21:38 +0530

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Home.css'
-import Navbar from './component/Navbar'
 import Body from './component/Body'
 
 function Home() {
   return (
-    <div>
-      <Body/>
+    <div className="home-container">
+      <main className="hero-section">
+        <Body />
+        <div className="cta-buttons">
+          <a href="/project" className="btn-primary">View My Work</a>
+          <a href="/About" className="btn-secondary">Technical Skills</a>
+        </div>
+      </main>
+      
+      {/* Optional: Add a subtle background decoration */}
+      <div className="background-blur"></div>
     </div>
   )
 }
