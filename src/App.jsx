da08eabc62c368e0a77da21df73787e283d4a3a3
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-12 07:41:59 +0530

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './component/Navbar'
-import Home from './Home'
-import About from './About'
-import Project from './Project'
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar /> {/* Now appears on every page */}
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/About' element={<About/>}/>
-        <Route path='/project' element={<Project/>} />
-      </Routes>
-    </div>
+    <div>App</div>
   )
 }
+
 export default App
