cdb76c308e5ddd476a8abf8fe599cf29a3760896
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-14 22:37:15 +0530

    navbar structure

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-main-container'>
+        <a href="" className="navbar-home"></a>
+        <a href="" className="navbar-about"></a>
+        <a href="" className="navbar-projects"></a>
+        <a href="" className="navbar-contact"></a>
+    </div>
   )
 }
 
