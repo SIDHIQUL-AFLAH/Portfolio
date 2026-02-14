b5649dfdcdc8e14bbf12cd369108837c801e622d
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-14 22:39:29 +0530

    home structure

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home-main-container'>
+        <div className="home-first-container"></div>
+    </div>
+  )
+}
+
+export default Home
