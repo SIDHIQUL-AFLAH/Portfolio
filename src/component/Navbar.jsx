47f9893f60f8412b886a20d8d91fe36f202b14a6
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-21 15:36:35 +0530

    delete

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -9,16 +9,6 @@ function Navbar() {
         <a href="" className="navbar-contact"></a>
 
 
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-        <div className="nav-container"></div>
-
 
     </div>
   )
