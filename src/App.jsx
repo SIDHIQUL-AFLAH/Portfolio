2e187db2106cbd2b3b8ae827021165d28ea67763
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-27 22:17:20 +0530

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import { Router } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='app-main-container'>
       <routes>
-        
+        <Route path='/'/>
       </routes>
     </div>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function Home() {
   return (
     <div className='home-main-container'>
-        <div className="home-first-container"></div>
+        <div className="home-first-container">
+          <div className="home-first-container-img">
+            {/* image in round shape */}
+          </div>
+          <div className="home-first-container-name">
+            <h2>Sidhiqul Aflah</h2>
+            <h3>Web Developer</h3>
+          </div>
+        </div>
         <div className="home-second-container"></div>
         <div className="home-third-container"></div>
 
