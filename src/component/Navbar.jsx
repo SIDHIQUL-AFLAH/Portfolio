6591f945c37cdce9474b031944c6493ae5c09ce4
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-02-27 14:52:49 +0530

    change

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='navbar-main-container'>
-        <a href="" className="navbar-home"></a>
-        <a href="" className="navbar-about"></a>
-        <a href="" className="navbar-projects"></a>
-        <a href="" className="navbar-contact"></a>
-
-
-
+    
+    <div>
+      <div className="navbar-first-container">
+        <h2>Sidhiqul Aflah</h2><h3> / Web Developer</h3>
+      </div>
+      <div className='navbar-second-container'>
+        <a href="" className="navbar-about">About Me</a>
+        <a href="" className="navbar-resume">Resume</a>
+        <a href="" className="navbar-projects">Project</a>
+        <a href="" className="navbar-contact">Contact</a>
     </div>
+    </div>
+    
+    
   )
 }
 
