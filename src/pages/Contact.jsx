aaf29bce97c28099b3c29f9b0b5c31a32327cba7
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-03-04 23:03:52 +0530

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="contact-first-container">
+        <h1>Contact page</h1>
+      </div>
+    </div>
   )
 }
 
