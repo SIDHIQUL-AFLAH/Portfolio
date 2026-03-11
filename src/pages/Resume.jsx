b6b726b49b79e46db37f593c4498f81d688db524
SIDHIQUL-AFLAH <sidhiqulaflah@gmail.com>
2026-03-11 19:12:52 +0530

    update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,14 +4,7 @@ const Resume = () => {
   return (
     <div>
       <div className="resume-first-container">
-        <div className="gggg"></div>
-        <div className="gggg"></div>
-        <div className="gggg"></div>
-        <div className="gggg"></div>
-        <div className="gggg"></div>
-        <div className="gggg"></div>
-        <div className="gggg"></div>
-        <div className="gggg"></div>
+       
       </div>
     </div>
   )
